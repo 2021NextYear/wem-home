081d1fd6cf1598b64396cea6745ad47e55b6a1e8
pishonTron <pishonTron@gmail.com>
2021-03-26 21:07:08 +0800

    init program

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+﻿$(window).scroll(function() {
+var scrollTop = $(this).scrollTop();
+if(scrollTop>50){
+$("#navbar").addClass("ea55_com_header");
+}else{
+$(".ea55_com_header").removeClass("ea55_com_header");
+}
+});
